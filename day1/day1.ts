439c88632c80ab02ccb91037ec4702a88c146cd5
ziebam <ziebamichal@tutanota.com>
2022-12-02 12:28:40 +0100

    Add .prettierrc

--- day1/day1.ts
+++ day1/day1.ts
@@ -1,27 +1,27 @@
 import fs from 'fs';
 
 const parseInput = () => {
-    return fs.readFileSync('day1/day1.txt', 'utf8').split(/\r?\n/);
-}
+  return fs.readFileSync('day1/day1.txt', 'utf8').split(/\r?\n/);
+};
 
 const computeSums = (input: string[]) => {
-    const totalCaloriesCarriedPerElf: number[] = [];
+  const totalCaloriesCarriedPerElf: number[] = [];
 
-    let sum = 0;
-    for (const calories of input) {
-        if (!calories) {
-            totalCaloriesCarriedPerElf.push(sum);
-            sum = 0;
-        } else {
-            sum += parseInt(calories);
-        }
+  let sum = 0;
+  for (const calories of input) {
+    if (!calories) {
+      totalCaloriesCarriedPerElf.push(sum);
+      sum = 0;
+    } else {
+      sum += parseInt(calories);
     }
+  }
 
-    return totalCaloriesCarriedPerElf.sort((a, b) => a - b).reverse();
-}
+  return totalCaloriesCarriedPerElf.sort((a, b) => a - b).reverse();
+};
 
 const input = parseInput();
 const sums = computeSums(input);
 
 console.log(`Part one: ${sums[0]}.`);
-console.log(`Part two: ${sums[0] + sums[1] + sums[2]}.`)
+console.log(`Part two: ${sums[0] + sums[1] + sums[2]}.`);
